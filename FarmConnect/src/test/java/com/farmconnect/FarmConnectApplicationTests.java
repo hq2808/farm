061adc484c595ff6c1b01de21562a20470ea8d3e
hunglk <hunglk1009@gmail.com>
2025-03-10 11:58:57 +0700

    init FarmConnect

---
+++ FarmConnect/src/test/java/com/farmconnect/FarmConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
